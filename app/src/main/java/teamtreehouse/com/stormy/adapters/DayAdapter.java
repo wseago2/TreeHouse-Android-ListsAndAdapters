53428a3ab9b46fa6ff7dc9f7ecfafa41b8ad0211
Wes Seago <wseago2@gmail.com>
2015-02-28 21:14:06 -0500

    Add HourlyForecastActivity.

--- app/src/main/java/teamtreehouse/com/stormy/adapters/DayAdapter.java
+++ app/src/main/java/teamtreehouse/com/stormy/adapters/DayAdapter.java
@@ -62,7 +62,13 @@ public class DayAdapter extends BaseAdapter {
 
         holder.iconImageView.setImageResource(day.getIconId());
         holder.temperatureLabel.setText(day.getTemperatureMax() + "");
-        holder.dayLabel.setText(day.getDayOfTheWeek());
+
+        if (position == 0) {
+            holder.dayLabel.setText("Today");
+        }
+        else {
+            holder.dayLabel.setText(day.getDayOfTheWeek());
+        }
 
         return convertView;
     }
